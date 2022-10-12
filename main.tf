bb4aa1a09922250cc15e9abaab660cd42317f431
Chandrasekar Srinivasan <76991374+chandrasekarsrin@users.noreply.github.com>
2022-10-12 12:13:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "oci_objectstorage_bucket" "test_bucket" {
-    #Required - devopsbuildprodtest
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaafkqteb2m6oygf6hr4wtpkf4g3il45zj26zsnhtx7umr54t7sxlga"
-    name = "terraform-test123"
-    namespace = "axsewmzamo2h"
+resource "oci_identity_compartment" "test_compartment" {
+    #Required
+    compartment_id = "ocid1.tenancy.oc1..aaaaaaaamwky26uqopqhzjegqc4qicwvrv6ar2jt6tl2dlqdsohehzw7nkiq"
+    description = "test compartment"
+    name = "chchsrin_to_be_Deleted"
 }
