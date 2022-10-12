18e620cdbd4e7c5f63e9dfb3b8332a73f02207c6
Chandrasekar Srinivasan <76991374+chandrasekarsrin@users.noreply.github.com>
2022-10-12 12:07:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-
-provider "oci" {
-  auth = "ResourcePrincipal"
-}
-
 resource "oci_objectstorage_bucket" "test_bucket" {
     #Required - devopsbuildprodtest
     compartment_id = "ocid1.compartment.oc1..aaaaaaaafkqteb2m6oygf6hr4wtpkf4g3il45zj26zsnhtx7umr54t7sxlga"
