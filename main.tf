453a0e0baa82b2ab400868d904d0ae456fe70624
Chandrasekar Srinivasan <76991374+chandrasekarsrin@users.noreply.github.com>
2022-10-12 14:18:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 data "oci_core_images" "test_images" {
     #Required
-    compartment_id = var.compartment_id
-
-    #Optional
-    display_name = var.image_display_name
-    operating_system = var.image_operating_system
-    operating_system_version = var.image_operating_system_version
-    shape = var.image_shape
-    state = var.image_state
-    sort_by = var.image_sort_by
-    sort_order = var.image_sort_order
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaafkqteb2m6oygf6hr4wtpkf4g3il45zj26zsnhtx7umr54t7sxlga"
 }
