5e87b7c9a8aa7de4984b0f153de9dbc38a3ba2d5
Chandrasekar Srinivasan <76991374+chandrasekarsrin@users.noreply.github.com>
2024-03-21 19:11:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ provider "oci" {
 resource "oci_objectstorage_bucket" "test_bucket" {
     #Required - devopsbuildprodtest
     compartment_id = "ocid1.compartment.oc1..aaaaaaaafkqteb2m6oygf6hr4wtpkf4g3il45zj26zsnhtx7umr54t7sxlga"
-    name = "terraform-test1"
+    name = "terraform-test15555"
     namespace = "axsewmzamo2h"
 }
